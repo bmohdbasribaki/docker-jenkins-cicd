a52667bb90ca40fc5145e2714988e80c2d6095c0
PST\basribaki <basribaki@commercedc.com.my>
2023-08-31 21:35:58 +0800

    second commit

--- src/main/java/index.java
+++ src/main/java/index.java
@@ -3,6 +3,7 @@ public class index {
 	
 	public static void main(String[] args) {
 		System.out.println("Test sync git and jenkins");
+		System.out.println("Test second commit git and jenkins");
 	}
 
 }
